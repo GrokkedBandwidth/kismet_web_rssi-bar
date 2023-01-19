0999468242d1ff630e9447e85a681b4d1129a178
theag <grokkedbandwidth@gmail.com>
2023-01-19 14:07:53 -0500

    Added Lock to target channel button

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from random import randint
 
 USERNAME = 'kismet'
 PASSWORD = 'kismet'
-IP = 'localhost'
+IP = '192.168.1.167'
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -15,6 +15,7 @@ Bootstrap(app)
 source_params = {
     'fields': [
         'kismet.datasource.channels',
+        'kismet.datasource.uuid'
     ]
 }
 
@@ -34,6 +35,7 @@ target_params = {
 mac = ''
 best_seen = -120
 last_seen = 0
+current_channel = 0
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
@@ -52,7 +54,7 @@ def set_mac():
 @app.route('/df', methods=['POST', 'GET'])
 def df():
     def generate():
-        global mac, best_seen, last_seen
+        global mac, best_seen, last_seen, current_channel
         while True:
             response = {}
             results = requests.post(
@@ -60,10 +62,10 @@ def df():
                 json=target_params
             ).json()
             rssi = results[0]['kismet.common.signal.last_signal']
-            channel = results[0]['kismet.device.base.channel']
+            current_channel = results[0]['kismet.device.base.channel']
             response[0] = 120 - (int(rssi) * -1)
             response[1] = rssi
-            response[2] = channel
+            response[2] = current_channel
             if rssi > best_seen:
                 best_seen = rssi
                 last_seen = time.time()
@@ -75,6 +77,19 @@ def df():
             time.sleep(.2)
     return Response(generate(), mimetype='text/event-stream')
 
+@app.route('/lock_channel', methods=['GET'])
+def lock_channel():
+    interface_list = []
+    results = requests.post(
+        url=f"http://{USERNAME}:{PASSWORD}@{IP}:2501/datasource/all_sources.json",
+        json=source_params).json()
+    for item in results:
+        interface_list.append(item['kismet.datasource.uuid'])
+    for item in interface_list:
+        requests.post(
+            url=f"http://{USERNAME}:{PASSWORD}@{IP}:2501/datasource/by-uuid/{item}/set_channel.cmd",
+            json={'channel': current_channel})
+    return results
 
 @app.route('/test', methods=['POST', 'GET'])
 def test():
