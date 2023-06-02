9f1a9de3caeb93c978b6fa0505a29a28002cd89c
theag <grokkedbandwidth@gmail.com>
2023-06-02 13:21:05 -0400

    Add index.js

---
+++ static/js/index.js
@@ -0,0 +1,19 @@
+function dfFunction() {
+    console.log("Opening the SSE connection")
+    var source = new EventSource("/df");
+    source.onmessage = function(event) {
+        sent_data = JSON.parse(event.data)
+
+        bar = "#prog_0"
+        $(bar).css('width', sent_data[0]+'%').attr('aria-valuenow', sent_data[0]);
+        lbar = "#prog_0_label"
+        $(lbar).text(sent_data[1]);
+        channel = "#channel"
+        $(channel).text('Current Channel: '+sent_data[2]);
+        best_seen = "#best"
+        $(best_seen).text('Best Seen: '+sent_data[3] + ' Time: ' + sent_data[4]);
+        current_rssi = "#current_rssi"
+        $(current_rssi).text('Current RSSI: '+sent_data[1]);
+    }
+}
+
