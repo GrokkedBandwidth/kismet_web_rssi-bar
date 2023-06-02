176587ee61ea998a9c970e3951db3e4084d08810
theag <grokkedbandwidth@gmail.com>
2023-06-02 15:55:54 -0400

    Add audio option

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,30 @@
+var mute = true;
+
+function toggleAudio() {
+    if (mute === true) {
+        mute = false;
+        $('#audio').text("Mute Audio");
+    } else {
+        mute = true;
+        $('#audio').text("Start Audio");
+    }
+}
+
+function adjustSound(oscillator, freq) {
+    oscillator.frequency.value = freq;
+}
+
 function dfFunction() {
     console.log("Opening the SSE connection")
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    const oscillator = audioCtx.createOscillator();
+    oscillator.type = "square";
+    oscillator.frequency.setValueAtTime(440, audioCtx.currentTime); // value in hertz
+    oscillator.start();
+    var localMute = true;
     var source = new EventSource("/df");
     source.onmessage = function(event) {
         sent_data = JSON.parse(event.data)
-
         bar = "#prog_0"
         $(bar).css('width', sent_data[0]+'%').attr('aria-valuenow', sent_data[0]);
         lbar = "#prog_0_label"
@@ -14,6 +35,14 @@ function dfFunction() {
         $(best_seen).text('Best Seen: '+sent_data[3] + ' Time: ' + sent_data[4]);
         current_rssi = "#current_rssi"
         $(current_rssi).text('Current RSSI: '+sent_data[1]);
+
+        if (mute === false) {
+            oscillator.connect(audioCtx.destination);
+            adjustSound(oscillator, sent_data[0]*8);
+        } else {
+            oscillator.disconnect(audioCtx.destination);
+        }
+
     }
 }
 
